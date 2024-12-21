e98cd9bf2a1a910d33620077d9925d18d2517a36
Vikram Kopuri <Vikram.Kopuri@intusurg.com>
2024-12-20 22:00:02 -0800

    major edit, script is now a python class

--- poison_apple_game.py
+++ poison_apple_game.py
@@ -1,91 +1,98 @@
 from random import choice
-
-def display_title():
-    title_art=r"""
-    __________      .__                               .___    _____                .__          
-    \______   \____ |__| __________   ____   ____   __| _/   /  _  \ ______ ______ |  |   ____  
-    |     ___/  _ \|  |/  ___/  _ \ /    \_/ __ \ / __ |   /  /_\  \\\____ \\\____ \|  | _/ __ \ 
-    |    |  (  <_> )  |\___ (  <_> )   |  \  ___// /_/ |  /    |    \  |_> >  |_> >  |_\  ___/ 
-    |____|   \____/|__/____  >____/|___|  /\___  >____ |  \____|__  /   __/|   __/|____/\___  >
-                            \/           \/     \/     \/          \/|__|   |__|             \/ 
-
-    """
-    print(title_art)
+class PoisonedApple:
+    def __init__(self):
+        #init variables
+        self.score = 0
+        self.best_score = 0
+        self.quit_game = False
+        self.game_no = 0
+        self.run()
     
+    def display_title(self):
+        title_art=r"""
+        __________      .__                               .___    _____                .__          
+        \______   \____ |__| __________   ____   ____   __| _/   /  _  \ ______ ______ |  |   ____  
+        |     ___/  _ \|  |/  ___/  _ \ /    \_/ __ \ / __ |   /  /_\  \\\____ \\\____ \|  | _/ __ \ 
+        |    |  (  <_> )  |\___ (  <_> )   |  \  ___// /_/ |  /    |    \  |_> >  |_> >  |_\  ___/ 
+        |____|   \____/|__/____  >____/|___|  /\___  >____ |  \____|__  /   __/|   __/|____/\___  >
+                                \/           \/     \/     \/          \/|__|   |__|             \/ 
 
-def random_bool():
-    return choice([True, False])
-
-def reset_apple(a):
-    a[0] = random_bool()
-    a[1] = not a[0]
-    return a
-
-def process_print_score(score, best_score):
-    if score > best_score:  # check for new high score
-        best_score = score
-
-    print(f'Score: {score} | Best Score: {best_score}\n')
-    return best_score
-
-def display_apples():
-    print("\n")
-    print("   🍎         🍏         ❌")  # ASCII art for two apples
-    print("  [ 1 ]      [ 2 ]      [ 0 ]")
-    print("\n")
-
-def get_input():
-    while True:
-        try:
-            display_apples()
-            #user_input = int(input('Input 1, 2 to select apple. 0 to quit (0, 1, or 2): '))
-            user_input = int(input('>'))
-            if user_input in [0, 1, 2]:
-                return user_input
-            else:
-                print("Invalid input. Please enter 0, 1, or 2.")
-        except ValueError:
-            print("Invalid input. Please enter a valid number (0, 1, or 2).")
-
-def main():
-    display_title()
-    print('Poison Apple Game')
-    print('=================')
-    print('Two apples: one is poison. Eat the wrong apple and die.')
-
-    score = 0
-    best_score = 0
-    quit_game = False
-    game_no = 0
-
-    while not quit_game:
-        # Start a new game
-        alive = True
-        score = 0
-        game_no += 1
-        print(f'New Game {game_no}')
+        """
+        print(title_art)
         
-        apple = [True, False]  # False is the poisoned apple
 
-        while alive:
-            # Reset apple positions
-            apple = reset_apple(apple)
-            user_input = get_input()
+    def random_bool(self):
+        return choice([True, False])
 
-            if user_input == 0:
-                quit_game = True
-                alive = False
-                best_score = process_print_score(score, best_score)
-                print('👋 Bye!!')
-            else:
-                alive = apple[user_input - 1]
-                if alive:
-                    score += 1
-                    print ('\U0001F60B Yummy')
+    def reset_apple(self):
+        self.apple[0] = self.random_bool()
+        self.apple[1] = not self.apple[0]
+        
+
+    def process_print_score(self):
+        if self.score > self.best_score:  # check for new high score
+            self.best_score = self.score
+
+        print(f'Score: {self.score} | Best Score: {self.best_score}\n')
+        
+
+    def display_apples(self):
+        print("\n")
+        print("   🍎         🍏         ❌")  # ASCII art for two apples
+        print("  [ 1 ]      [ 2 ]      [ 0 ]")
+        print("\n")
+
+    def get_input(self):
+        while True:
+            try:
+                self.display_apples()
+                #user_input = int(input('Input 1, 2 to select apple. 0 to quit (0, 1, or 2): '))
+                self.user_input = int(input('>'))
+                if self.user_input in [0, 1, 2]:
+                    return self.user_input
                 else:
-                    print('\U0001F480 You ate a poisoned apple!! You died. \U0001F480')
-                
-                best_score = process_print_score(score, best_score)
+                    print("Invalid input. Please enter 0, 1, or 2.")
+            except ValueError:
+                print("Invalid input. Please enter a valid number (0, 1, or 2).")
+
+    def run(self):
+        self.display_title()
+        print('Poison Apple Game')
+        print('=================')
+        print('Two apples: one is poison. Eat the wrong apple and die.')
+
+
+
+        while not self.quit_game:
+            # Start a new game
+            self.alive = True
+            self.score = 0
+            self.game_no += 1
+            print(f'New Game {self.game_no}')
+            
+            self.apple = [True, False]  # False is the poisoned apple
+
+            while self.alive:
+                # Reset apple positions
+                self.reset_apple()
+                # player input
+                self.get_input()
+
+                if self.user_input == 0:
+                    self.quit_game = True
+                    self.alive = False
+                    self.process_print_score()
+                    print('👋 Bye!!')
+                else:
+                    self.alive = self.apple[self.user_input - 1]
+                    if self.alive:
+                        self.score += 1
+                        print ('\U0001F60B Yummy')
+                    else:
+                        print('\U0001F480 You ate a poisoned apple!! You died. \U0001F480')
+                    
+                    self.process_print_score()
 
 if __name__ == "__main__":
-    main()
+    my_game=PoisonedApple()
+    
